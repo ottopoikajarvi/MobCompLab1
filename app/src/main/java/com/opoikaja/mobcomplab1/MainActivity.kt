9a21cc56855c59cb582c55b51b5b7bc1cfe44414
ottopoikajarvi <tootsi666@gmail.com>
2020-02-04 09:52:25 +0200

    Lab 4 done

--- app/src/main/java/com/opoikaja/mobcomplab1/MainActivity.kt
+++ app/src/main/java/com/opoikaja/mobcomplab1/MainActivity.kt
@@ -3,7 +3,11 @@ package com.opoikaja.mobcomplab1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,10 +41,35 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val data = arrayOf("Oulu", "Helsinki", "Tampere")
-        val reminderadapter = ReminderAdapter(applicationContext, data)
-        list.adapter = reminderadapter
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        refreshList()
+    }
+
+    private fun refreshList() {
+        doAsync {
+            val db = Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java,
+                "reminders"
+            ).build()
+            val reminders = db.reminderDao().getReminders()
+            db.close()
+
+            uiThread {
+                if (reminders.isNotEmpty()) {
+
+                    val adapter = ReminderAdapter(applicationContext, reminders)
+                    list.adapter = adapter
+                } else{
+                    toast("No reminders yet")
+                }
+            }
+        }
 
     }
 }
